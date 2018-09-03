b9627a71898bcf291cbc4dd1daa58d82663b5f03
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-09-03 15:04:29 +0100

    change of chart bars number
    - set a new design
    - set color of every bar to unique color
    - show of 5 bars
    - set next-previous buttons

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 $(function () {
-    getPlayerScores();
+    getPlayersScores();
+    range=0;
 });
 
-function getPlayerScores() {
+
+function getPlayersScores() {
     $.get("http://cdn.55labs.com/demo/api.json", function (data) {
-        $("#title").html(data.settings.label);
-        for (var player in data.data.DAILY.dataByMember.players) {
-            var score = data.data.DAILY.dataByMember.players[player].points[0];
-            if (score != null) {
-                var percentage = Math.floor(score * 100 / 1000);
-                var date = data.data.DAILY.dates[0]
-                date = date.slice(0, 4) + "-" + date.slice(5, 6) + "-" + date.slice(7, 8);
-                $("#graph").append("<dd class='percentage percentage-"
-                    + percentage + "'><span class='text'>" + player + " " + date + "</span></dd>");
-             }
+        var players = getPlayers(data);
+        var dates = getDates(data, range);
+
+        for (dateIndex in dates) {
+            var i = 0;
+            for (var player in players) {
+                i++;
+                var score = players[player].points[dateIndex];
+                if (score !== null) {
+                    var percentage = Math.floor(score * 100 / 1000);
+                    var date = dates[dateIndex]
+                    date = date.slice(6, 8) + "-" + date.slice(4, 6) + "-" + date.slice(0, 4);
+                    $(".chart").append("<div class='bar bar-" +
+                        percentage + "'><span class='date'>"+date+"</span></div>");
+                    var playerBarColor = "#" + intToRGB(hashCode(player));
+                    $(".chart > div:nth-child("+ i +"n+1)").css("background-color", playerBarColor);
+                }
+            }
+            var playersNumber = Object.keys(players).length;
+            $(".chart > div:nth-child("+ playersNumber +"n+1)").css("margin-bottom", "10px");
         }
+
+        $('div.bar').hover(function(){
+            $(this).find(".date").css('visibility', 'visible');
+        },    
+        function(){
+            $(this).find(".date").css('visibility', 'hidden');
+        });
     });
+
+    
+}
+
+function getPlayers(data) {
+    var players = data.data.DAILY.dataByMember.players;
+    return players;
+}
+
+function getDates(data, range) {
+    var dates = data.data.DAILY.dates;
+    var len = Object.keys(dates).length;
+    dates = dates.slice(range*5, Math.min((range+1)*5, len) + 1);
+    return dates;
 }
 
 
